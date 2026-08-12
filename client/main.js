4de60f38676a1f9793c98950d10c24c426ca4fa1
nemeancreates <nemeancreates@gmail.com>
2026-08-12 11:06:16 -0700

    v0.1.41 - solo-aware clip-clamped message, remove broken Switch Monitor button, recording-stopped listener, highlights column scroll fix

--- client/main.js
+++ client/main.js
@@ -444,8 +444,8 @@ const RESOLUTION_MAP = {
 };
 
 const LATEST_CLIENT_VERSION = {
-  version: '0.1.40',
-  downloadUrl: 'https://peakbu-media.nyc3.cdn.digitaloceanspaces.com/releases/PeakAbu-Setup-0.1.40.exe',
+  version: '0.1.41',
+  downloadUrl: 'https://peakbu-media.nyc3.cdn.digitaloceanspaces.com/releases/PeakAbu-Setup-0.1.41.exe',
   releaseNotes: 'Discord Integration.'
 };
 
